9321c5cfeb330301be368282448c5e3ab067d236
D4v1d_D4art3 <jesusdavidduartejimenez@gmail.com>
2020-12-09 23:16:06 -0500

    homePage

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, HostListener} from "@angular/core";
 import {Router} from "@angular/router";
-import {AuthService} from "../../../core/service/auth/auth.service";
+import {AuthService} from "@core/service/auth/auth.service";
 
 @Component({
   selector: "app-header",
@@ -11,6 +11,15 @@ export class HeaderComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
+  scroll: boolean = false;
+  @HostListener("document:scroll")
+  scrollFunction() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.scroll = true;
+    } else {
+      this.scroll = false;
+    }
+  }
   logOut() {
     this.authService.logout().then(() => {
       this.router.navigate(["auth/login"]);
